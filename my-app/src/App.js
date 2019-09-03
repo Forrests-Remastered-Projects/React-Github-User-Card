4661af0347320df81c4ea9dedf13b4b399887dc5
Forrest-Darabian <mr.smiley72@yahoo.com>
2019-09-03 12:46:27 -0700

    Github info is rendering on page

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -10,17 +10,32 @@ class App extends React.Component {
   }
 
 componentDidMount() {
+  console.log("First Render (mounting)");
   fetch('http://api.github.com/users/forrestdarabian')
     .then(res => res.json())
-    .then(data => console.log(data));
+    .then(data => this.setState({ user: data }));
+}
+
+componentDidUpdate() {
+  console.log(this.state);
 }
 
   render() {
     return (
       <div className="App">
+        <UserCard user={this.state.user} />
       </div>
     );
   }
+}
+
+function UserCard(props) {
+  return (
+  <div>
+    <h2>{props.user.login}</h2>
+    <p>{props.user.location}</p>
+  </div>
+  );
 }
 
 export default App;
